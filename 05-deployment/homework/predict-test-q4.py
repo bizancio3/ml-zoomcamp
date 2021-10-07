abce976de9ac2b9f8ca21188a85ffee8f637439a
Javier Villarrubia <bizancio3@gmail.com>
2021-10-07 19:27:28 +0200

    completed homework w5

---
+++ 05-deployment/homework/predict-test-q4.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import requests
+
+
+url = 'http://localhost:9696/predict'
+
+customer_id = 'question4'
+customer = {
+    "contract": "two_year", 
+    "tenure": 1, 
+    "monthlycharges": 10
+}
+
+
+response = requests.post(url, json=customer).json()
+print(response)
---
+++ 05-deployment/homework/question3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pickle
+
+customer = {
+    "contract": "month-to-month",
+    "tenure": 12,
+    "monthlycharges": 19.7
+}
+
+model_file = 'model1.bin'
+
+with open(model_file, 'rb') as f_in:
+    dv, model = pickle.load(f_in)
+
+
+def predict():
+
+    X = dv.transform([customer])
+    y_pred = model.predict_proba(X)[0, 1]
+    churn = y_pred >= 0.5
+
+    print(round(float(y_pred), 4), bool(churn))
+ 
+    return y_pred, churn
+    
+
+if __name__ == "__main__":
+    predict()
+    
+
